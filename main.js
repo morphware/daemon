2066128d5a2d0bad5a179e0f0ce50e7e8f924da2
Kenso Trabing <ktrabing@acm.org>
2021-06-08 16:55:58 -0400

    Change ABI pathname

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs         = require('fs');
 const path       = require('path');
 const bodyParser = require('body-parser');
@@ -35,20 +36,15 @@ app.use(express.json());
 const webtorrent = new WebTorrent();
 
 ///////////////////////////////////////////////////////////////////////////////
-// FIXME 0
 const provider = new Web3.providers.HttpProvider('http://localhost:8545');
 let web3 = new Web3(provider);
 
+// TODO Un-hardcode these three
 const jobFactoryContractAddress = '0xC89Ce4735882C9F0f0FE26686c53074E09B0D550';
 const account4Address = '0xd03ea8624C8C5987235048901fB614fDcA89b117';
-
-// FIXME 1
-// const abiPathname = '\\\wsl$\\Ubuntu\\home\\wsladmin\\Morphware\\learn\\build\\contracts\\JobFactory.json';
-const abiPathname = 'JobFactory.json';
-
+const abiPathname = 'JobFactory-copyABI.json';
 
 let jobFactoryAbi = JSON.parse(fs.readFileSync(path.resolve(abiPathname),'utf-8')).abi;
-
 let jobFactoryContract = new web3.eth.Contract(jobFactoryAbi,jobFactoryContractAddress);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -57,15 +53,8 @@ app.post('/upload', validFields, async function (req, res) {
 
     // TODO Incorporate `fileFilter`
 
-    // TODO Use `.size` to gauge the space complexity of the runtime (see: `index.html` in `client`)
-    //      and send the data (in gigabytes or whatever fits in `uint8`)
-
     ///////////////////////////////////////////////////////////////////////////////
-
     const fieldsObj = JSON.parse(req.body.fields);
-
-    // TEST
-    // console.log(req.files['training-data'][0].size);
 
     jobFactoryContract.methods.postJobDescription(
         parseInt(fieldsObj['training-time']),
@@ -80,7 +69,6 @@ app.post('/upload', validFields, async function (req, res) {
     );
 
     // TODO return next(error);
-
 
     ///////////////////////////////////////////////////////////////////////////////
     for (const [_fieldname, _fileArray] of Object.entries(req.files)) {
