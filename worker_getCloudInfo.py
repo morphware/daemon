6b9858b45fa0e4350accaaa9589b9d6dd55be1e5
Kenso Trabing <ktrabing@acm.org>
2021-06-11 12:25:18 -0400

    Write GPU VPS price-getter

---
+++ worker_getCloudInfo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+
+
+class URI:
+
+    def __init__(self):
+        self.scheme   = 'http'
+        if self.scheme != 'http':
+            # TODO May be some SSL-related parameterization in `cloudinfo`'s set-up script that needs to be done
+            pass
+        self.endpoint = self.scheme+'://'+'localhost:8000/api/v1'
+
+
+class C:
+
+    def __init__(self):
+        self.endpoint = URI()
+
+    def get_gpu_instance_prices(self,provider,service):
+        uri = self.endpoint
+                +f'/providers/{provider}'
+                +f'/services/{service}'
+                +f'/regions/{region}'
+                +f'/products'
+        metadata = json.loads(requests.get(uri).text)
+        l = []
+        for instanceType in metadata['products']:
+            if instanceType.get('gpusPerVm') > 0:
+                l.append(instanceType)
+        return l
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    c = C()
+    c.get_gpu_instance_prices('amazon','compute')
