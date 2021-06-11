c8f8a185724275a035d06b17e58a82b2271100d8
Kenso Trabing <ktrabing@acm.org>
2021-06-11 12:02:29 -0400

    Move notes on idling functionality to daemon repo

---
+++ idle.rs
@@ -0,0 +1,14 @@
+// Scheduler for when there aren't any good jobs for this co-processor
+
+// Bitcoin Mining
+
+// Ethereum Mining
+
+// Ravencoin Mining
+
+// LivePeer Transcoding
+
+// BitTorrent Seeding (viz. BTT)
+
+// Unminable Running
+
