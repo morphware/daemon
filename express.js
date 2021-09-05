a5a24bd2d909422b8245cfa02c6edd50ae663d40
William Mantly <wmantly@gmail.com>
2021-09-05 14:05:20 -0400

    Added error catching to express app

--- express.js
+++ express.js
@@ -15,4 +15,26 @@ app.listen(conf.httpPort, () => {
     console.log(`Server running at http://localhost:${conf.httpPort}`);
 });
 
+// Catch 404 and forward to error handler. If none of the above routes are
+// used, this is what will be called.
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.message = 'Page not found'
+  err.status = 404;
+  next(err);
+});
+
+// Error handler. This is where `next()` will go on error
+app.use(function(err, req, res, next) {
+  console.error(err.status || res.status, err.name, req.method, req.url);
+  if(![401, 404].includes(err.status || res.status)){
+    console.error(err.message);
+    console.error(err.stack);
+    console.error('=========================================');
+  }
+
+  res.status(err.status || 500);
+  res.json({name: err.name, message: err.message});
+});
+
 module.exports = app;
