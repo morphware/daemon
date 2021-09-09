14b026314a6d0d8b2299f4d5f5deabcb2df98702
Kenso Trabing <ktrabing@acm.org>
2021-09-09 15:35:34 -0400

    Update addresses: Add jobPoster to Bid struct. #19

--- conf/development.js
+++ conf/development.js
@@ -1,7 +1,7 @@
 module.exports = {
 	morphwareTokenContractAddress: '0xbc40e97e6d665ce77e784349293d716b030711bc',
-	auctionFactoryContractAddress: '0x3e8fC2ecb1485698C13b96Ee28602c65b0A153De',
-	jobFactoryContractAddress: '0xE608E006add349B181D043483eA3840a2f4B6778',
+	auctionFactoryContractAddress: '0xD2c0251006d7e6C411DFEf1232d5a66859B8a720',
+	jobFactoryContractAddress: '0x85feb7D97C3fc25DF24eC22EEa02c360dF3c61A2',
 	isGPUnode: true,
 	ethAddress: 'wss://ropsten.infura.io/ws/v3/dc53ba9a23564600bfbe5f8c2f345d1d',
 	// ethAddress: 'wss://geth.vm42.us'
