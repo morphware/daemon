19802f756f8a0f97275c69d3b1194a6d21d08063
Darshan <darshan.raju99@gmail.com>
2021-09-15 07:19:56 +1000

    Add wallet daemon callbacks to daemon class

--- frontend/src/service/DaemonService.ts
+++ frontend/src/service/DaemonService.ts
@@ -16,14 +16,85 @@ export interface ActiveTorrents {
   torrents: Array<TorrentData>;
 }
 
+interface WalletBalanceProps {
+  balance: string;
+  address: string;
+}
+
+type TransactionProps = any;
+
+//TODO: write out entire transaction interface
+interface WalletHistoryProps {
+  transactions: Array<TransactionProps>;
+  address: string;
+}
+
+interface SendMWTRequestProps {
+  address: string;
+  amount: string;
+  gas: string;
+}
+
 export interface IDaemonService {
   submitTrainModelRequest(modelRequest: ITrainingModelValuesV2): Promise<void>;
   getActiveTorrents(): Promise<ActiveTorrents>;
+  getMWTBalance(): Promise<WalletBalanceProps>;
+  getWalletHistory(): Promise<WalletHistoryProps>;
+  sendMWT(sendMWTRequest: SendMWTRequestProps): Promise<TransactionProps>;
 }
 export class DaemonService implements IDaemonService {
   private readonly baseUrl: string = "http://127.0.0.1:3001";
 
   constructor() {}
+
+  public sendMWT = async (
+    sendMWTRequest: SendMWTRequestProps
+  ): Promise<any> => {
+    const url = `${this.baseUrl}/wallet/send`;
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(sendMWTRequest),
+    };
+
+    const response = await fetch(url, requestOptions);
+    const transaction: TransactionProps = await response.json();
+
+    console.log("transaction: ", transaction);
+
+    return transaction;
+  };
+
+  public getWalletHistory = async (): Promise<WalletHistoryProps> => {
+    const url = `${this.baseUrl}/wallet/history`;
+
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const response = await fetch(url, requestOptions);
+    const history: WalletHistoryProps = await response.json();
+    console.log("History: ", history);
+
+    return history;
+  };
+
+  public getMWTBalance = async (): Promise<WalletBalanceProps> => {
+    const url = `${this.baseUrl}/api/v0/wallet`;
+
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const response = await fetch(url, requestOptions);
+    const balance: WalletBalanceProps = await response.json();
+    console.log("Balance: ", balance);
+
+    return balance;
+  };
 
   public submitTrainModelRequest = async (
     trainModelRequest: ITrainingModelValuesV2
