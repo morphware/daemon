8c6683c4d5e712da51c1521f86b9b3afeb065a04
Darshan <darshan.raju99@gmail.com>
2021-09-24 08:33:24 +1000

    add ipcrenderer to window

--- devRun.js
+++ devRun.js
@@ -88,6 +88,8 @@ function startElectron(){
 			localStorage.setItem('url', "${conf.httpAddress}:${conf.httpPort}")
 			localStorage.setItem('environment', "${conf.environment}")
 			const ipcRenderer = window.require("electron").ipcRenderer;
+			console.log("renderer: ", ipcRenderer);
+			window.renderer = ipcRenderer;
 		`);
 		
 		let child = spawn('npx', ['nodemon', '-w', 'electron.js', '--exec', 'electron', '.'], {
