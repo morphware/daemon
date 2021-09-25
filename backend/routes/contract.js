4b4f346bf867a206799963175be05b602a1d12a9
William Mantly <wmantly@gmail.com>
2021-09-25 16:07:01 -0400

    API for getting job info

--- backend/routes/contract.js
+++ backend/routes/contract.js
@@ -2,6 +2,7 @@
 
 const router = require('express').Router();
 const {JobPoster} = require('../model/jobPoster');
+const {Job} = require('../model/job');
 
 // The wallet should be injected into the req obj
 const {wallet} = require('../model/morphware');
@@ -12,7 +13,26 @@ router.post('/', async function (req, res, next) {
 
 		let job = await JobPoster.new({...req.body, wallet});
 
-		return res.json({status: 'success', job: job.jobData.returnValues.id});
+		return res.json({status: 'success', job: job.id});
+
+	}catch(error){
+		next(error);
+	}
+});
+
+router.get('/', async function(req, res, next){
+	try{
+		let jobs = {};
+
+		for(let [id, job] of Object.entries(Job.jobs)){
+			jobs[id] = {
+				type: job.jobType,
+				jobData: job.jobData,
+				transactions: job.transactions,
+			}
+		}
+
+		return res.json({jobs});
 
 	}catch(error){
 		next(error);
