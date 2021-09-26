4d38d84bd893db3a6b1aedf5afa81f76367fa5ef
William Mantly <wmantly@gmail.com>
2021-09-25 20:07:34 -0400

    Updated contract address

--- backend/conf/development.js
+++ backend/conf/development.js
@@ -2,9 +2,9 @@ module.exports = {
 	morphwareTokenABIPath: 'MorphwareToken-RopstenABI',
 	morphwareTokenContractAddress: '0xbc40e97e6d665ce77e784349293d716b030711bc',
 	
-	// https://ropsten.etherscan.io/address/0x0f96cf29c8d9f65f83e6992cad3ebbe9a395f332#code
+	// https://ropsten.etherscan.io/address/0xC2B92AB1dc450289857B22606bE2eEAB4b8a7EFc#code
 	auctionFactoryABIPath: 'VickreyAuction-RopstenABI',
-	auctionFactoryContractAddress: '0x0f96cf29c8d9f65f83e6992cad3ebbe9a395f332',
+	auctionFactoryContractAddress: '0xC2B92AB1dc450289857B22606bE2eEAB4b8a7EFc',
 	
 	
 	// https://ropsten.etherscan.io/address/0xb2da7fcc212fe1c747048d7c7caca6a2bd8ec0bc#code
