012427b245382b40dce06fde699a2fe96547e0bd
Darshan <darshan.raju99@gmail.com>
2021-12-18 14:56:23 +1100

    Updated contract addresses for mainnet

--- backend/conf/production.js
+++ backend/conf/production.js
@@ -1,24 +1,19 @@
 module.exports = {
+  // https://etherscan.io/address/0x5f80d31044d8b15acffa3454dffda9f022453461#code
+  morphwareTokenABIPath: "MorphwareToken-MainnetABI",
+  morphwareTokenContractAddress: "0x5f80d31044d8b15acffa3454dffda9f022453461",
 
-	// https://etherscan.io/address/0x5f80d31044d8b15acffa3454dffda9f022453461#code
-	morphwareTokenABIPath: 'MorphwareToken-MainnetABI',
-	morphwareTokenContractAddress: '0x5f80d31044d8b15acffa3454dffda9f022453461',
-	
-	// https://ropsten.etherscan.io/address/0x0f96cf29c8d9f65f83e6992cad3ebbe9a395f332#code
-	auctionFactoryABIPath: 'VickreyAuction-RopstenABI',
-	auctionFactoryContractAddress: '0x0f96cf29c8d9f65f83e6992cad3ebbe9a395f332',
-	
-	
-	// https://ropsten.etherscan.io/address/0xb2da7fcc212fe1c747048d7c7caca6a2bd8ec0bc#code
-	jobFactoryAbiPath: 'JobFactory-RopstenABI',
-	jobFactoryContractAddress: '0xb2da7fcc212fe1c747048d7c7caca6a2bd8ec0bc',
-	
-	ethAddress: 'wss://mainnet.infura.io/ws/v3/b953c0b0ccd34ab69751fb492c9c5f04',
-	// ethAddress: 'wss://geth.vm42.us',
+  // https://ropsten.etherscan.io/address/0x0f96cf29c8d9f65f83e6992cad3ebbe9a395f332#code
+  auctionFactoryABIPath: "VickreyAuction-RopstenABI",
+  auctionFactoryContractAddress: "0x094FC2c1fa685977F608ec5B0a571fc8d9eC9273",
 
-	// Turn on chrome dev tools from electron app. 
-	electronDev: false,
+  // https://ropsten.etherscan.io/address/0xb2da7fcc212fe1c747048d7c7caca6a2bd8ec0bc#code
+  jobFactoryAbiPath: "JobFactory-RopstenABI",
+  jobFactoryContractAddress: "0xb2da7fcc212fe1c747048d7c7caca6a2bd8ec0bc",
+
+  ethAddress: "wss://mainnet.infura.io/ws/v3/b953c0b0ccd34ab69751fb492c9c5f04",
+  // ethAddress: 'wss://geth.vm42.us',
+
+  // Turn on chrome dev tools from electron app.
+  electronDev: false,
 };
-
-
-
