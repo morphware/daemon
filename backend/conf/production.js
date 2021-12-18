d7725f3dc54249c26cdd1dcb5cd08fac9e0a478b
Darshan <darshan.raju99@gmail.com>
2021-12-18 15:07:22 +1100

    Added deployed ABI's

--- backend/conf/production.js
+++ backend/conf/production.js
@@ -4,11 +4,11 @@ module.exports = {
   morphwareTokenContractAddress: "0x5f80d31044d8b15acffa3454dffda9f022453461",
 
   // https://etherscan.io/address/0x03e99193e929535133afde27f293826783908109#code
-  auctionFactoryABIPath: "VickreyAuction-RopstenABI",
+  auctionFactoryABIPath: "VickreyAuction-MainnetABI",
   auctionFactoryContractAddress: "0x03e99193e929535133afde27f293826783908109",
 
   // https://etherscan.io/address/0x094FC2c1fa685977F608ec5B0a571fc8d9eC9273#code
-  jobFactoryAbiPath: "JobFactory-RopstenABI",
+  jobFactoryAbiPath: "JobFactory-MainnetABI",
   jobFactoryContractAddress: "0x094FC2c1fa685977F608ec5B0a571fc8d9eC9273",
 
   ethAddress: "wss://mainnet.infura.io/ws/v3/b953c0b0ccd34ab69751fb492c9c5f04",
